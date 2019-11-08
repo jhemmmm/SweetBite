a6f10f147389a73af6be0561947f78b2e02eb8ac
Sweeeeeep <13380637+Sweeeeep@users.noreply.github.com>
2019-11-08 20:22:13 +0800

    update image slider

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,13 +14,13 @@
         </ol>
         <div class="carousel-inner" role="listbox">
             <div class="carousel-item active">
-            <img class="d-block img-fluid" src="http://placehold.it/1200x350" alt="First slide">
+            <img class="d-block img-fluid" src="/images/slider/1.png" alt="First slide">
             </div>
             <div class="carousel-item">
-            <img class="d-block img-fluid" src="http://placehold.it/1200x350" alt="Second slide">
+            <img class="d-block img-fluid" src="/images/slider/2.png" alt="Second slide">
             </div>
             <div class="carousel-item">
-            <img class="d-block img-fluid" src="http://placehold.it/1200x350" alt="Third slide">
+            <img class="d-block img-fluid" src="/images/slider/3.png" alt="Third slide">
             </div>
         </div>
         <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
